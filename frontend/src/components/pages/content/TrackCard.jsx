8f91f203996b43cb90d85498fdda6d5d053b6299
Jinyi Ge <jge34@wisc.edu>
2025-08-15 20:36:52 -0500

    fixed a bug that causing track card does not display artist name.

--- frontend/src/components/pages/content/TrackCard.jsx
+++ frontend/src/components/pages/content/TrackCard.jsx
@@ -10,11 +10,25 @@ export default function TrackCard({ track }) {
   // const { playlists, addTrackTo } = useContext(PlaylistContext);
 
   const to = `/songs/${track.track_id}`;
+  let artists = [];
+  try {
+    artists = track.artists ? JSON.parse(track.artists) : [];
+  } catch {
+    artists = [];
+  }
+
+  const subtitle =
+    artists.length > 0
+      ? artists
+        .map((artist) => artist.artist_name || "(Unknown Artist)")
+        .join(", ")
+      : "(No Artist Info)";
+
   return (
     <Link to={to} className="track-tile text-reset text-decoration-none" aria-label={`Open ${track.track_name}`}>
       <div className="tile-text">
         <div className="tile-title">{track.track_name ?? "(Untitled)"}</div>
-        <div className="tile-subtitle">{track.artist_names ?? "(Unknown artist)"}</div>
+        <div className="tile-subtitle">{subtitle}</div>
       </div>
     </Link>
   );
