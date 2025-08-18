dcc5b90acd0329a4fddb0ab6696feca5a4d3211a
Jinyi Ge <jge34@wisc.edu>
2025-08-17 20:21:11 -0500

    Temp version for check

--- backend/script/migrate.js
+++ backend/script/migrate.js
@@ -10,12 +10,13 @@ const __dirname  = path.dirname(__filename);
 
 // adjust these if your paths differ
 const DB_FILE   = path.resolve(__dirname, '../sql/spotify.db');
-// const MIGRATION = path.resolve(__dirname, '../sql/2025-08-15_add_seed_flags.sql');
-const MIGRATION = path.resolve(__dirname, '../sql/fix_created_at.sql');
+const MIGRATION1 = path.resolve(__dirname, '../sql/2025-08-15_add_seed_flags.sql');
+const MIGRATION2 = path.resolve(__dirname, '../sql/fix_created_at.sql');
 
 
 async function main() {
-  const sql = await fs.readFile(MIGRATION, 'utf8');
+  const sql1 = await fs.readFile(MIGRATION1, 'utf8');
+  const sql2 = await fs.readFile(MIGRATION2, 'utf8');
   const db = await open({ filename: DB_FILE, driver: sqlite3.Database });
 
   // Good practice for schema changes
@@ -23,7 +24,8 @@ async function main() {
   await db.exec('BEGIN;');
 
   try {
-    await db.exec(sql);
+    await db.exec(sql1);
+    await db.exec(sql2);
     await db.exec('COMMIT;');
     console.log('Migration applied successfully.');
   } catch (err) {
